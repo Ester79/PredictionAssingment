a09f956a053f4c968c00ada6a1a2fa4326ce44a4
Ester79 <esterramoscuervo@hotmail.com>
2020-09-09 21:41:45 +0200

    update 1

--- Code.R
+++ Code.R
@@ -1,3 +1,15 @@
 
 # Code 
+getwd()
+
+# Libraries necessaries
+library(caret)
+library(rpart)
+library(rpart.plot)
+library(RColorBrewer)
+library(rattle)
+library(randomForest)
+library(corrplot)
+library(gbm)
+library(readxl)
 
